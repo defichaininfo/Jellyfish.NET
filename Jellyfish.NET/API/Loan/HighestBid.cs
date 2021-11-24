176ae3735f0f3db5c3af141529ac9cdce940447e
defichaininfo <develop@defichaininfo.de>
2021-11-24 22:01:07 +0100

    Add highestBid object to listVaults, getVault and listAuctions RPCs (#850)

---
+++ Jellyfish.NET/API/Loan/HighestBid.cs
@@ -0,0 +1,10 @@
+﻿namespace Jellyfish.API.Loan;
+
+public class HighestBid
+{
+    /// <summary>
+    /// amount@symbol
+    /// </summary>
+    public string Amount { get; init; } = string.Empty;
+    public string Owner { get; init; } = string.Empty;
+}
--- Jellyfish.NET/API/Loan/VaultLiquidationBatch.cs
+++ Jellyfish.NET/API/Loan/VaultLiquidationBatch.cs
@@ -5,4 +5,5 @@ public class VaultLiquidationBatch
     public double Index { get; init; }
     public string[] Collaterals { get; init; } = Array.Empty<string>();
     public string Loan { get; init; } = string.Empty;
+    public HighestBid? HighestBid { get; init; }
 }
