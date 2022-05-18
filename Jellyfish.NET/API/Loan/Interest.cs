2a34896faac97df2f2cb74c090a84383c6d35279
defichaininfo <develop@defichaininfo.de>
2022-05-18 16:25:06 +0200

    update test & interfaces to support defid:2.6.x (#947)

--- Jellyfish.NET/API/Loan/Interest.cs
+++ Jellyfish.NET/API/Loan/Interest.cs
@@ -3,6 +3,7 @@
 public class Interest
 {
     public string Token { get; init; } = string.Empty;
+    public decimal RealizedInterestPerBlock { get; init; }
     public decimal TotalInterest {  get; init; }
     public decimal InterestPerBlock { get; init; }
 }
