cf86f1620b130155a9e4ae4a705c31f6d91bc1f7
defichaininfo <develop@defichaininfo.de>
2022-05-21 17:00:08 +0200

    fix(jellyfish-api-core): add `hex` field in AccountOwner interface (#1083)

--- Jellyfish.NET/API/Account/AccountOwner.cs
+++ Jellyfish.NET/API/Account/AccountOwner.cs
@@ -6,6 +6,7 @@ public class AccountOwner
 {
     public string Asm { get; init; } = string.Empty;
     [JsonProperty("reqSigs")]
+    public string Hex { get; init; } = string.Empty;
     public int RequiredSignatures { get; init; }
     public string Type { get; init; } = string.Empty;
     public string[] Addresses { get; init; } = Array.Empty<string>();
